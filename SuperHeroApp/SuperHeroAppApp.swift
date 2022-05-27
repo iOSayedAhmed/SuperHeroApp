c061d62dbab5fb7b605dce2324bfbc65d438eba8
iOSayedAhmed <elsayed1ahmed0@gmail.com>
2022-05-27 10:26:10 +0200

    Initial Commit

---
+++ SuperHeroApp/SuperHeroAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroAppApp.swift
+//  SuperHeroApp
+//
+//  Created by Develop on 27/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
